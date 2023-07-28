0af6d330efd011c5b653e6b854743e1c67e61d5e
Fabrizio Giffi <fagi@solute.de>
2023-07-28 17:05:52 +0200

    Chapter 10 - Page 191. Advanced routing WIP

--- app/routes/bands/band.js
+++ app/routes/bands/band.js
@@ -3,7 +3,7 @@ import { service } from '@ember/service';
 
 export default class BandsBandRoute extends Route {
   @service catalog;
-  
+
   model(params) {
     return this.catalog.find('band', (band) => band.id === params.id);
   }
